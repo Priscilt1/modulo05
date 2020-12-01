2af005547c7c2dfc3872d87fe3a2ecddc5d471dc
Priscila Ribeiro <priscilt.ribeiro@hotmail.com>
2020-12-01 12:10:40 -0300

    exportando com method shorthand ES6

--- scr/app/controllers/instructors.js
+++ scr/app/controllers/instructors.js
@@ -1,139 +1,46 @@
-const fs = require('fs')
-const data = require("../data.json")
-const { age, date } = require('../utils')
+const { age, date } = require('../../lib/utils')
 const Intl = require('intl')
 
-// index
-exports.index = function (req, res) {
-    return res.render("instructors/index", {instructors: data.instructors})
-}
+module.exports = {
+    index(req,res){
+        return res.render("instructors/index")
+    },
 
-//create
-exports.create = function (req, res) {
-    return res.render ('instructors/create')
-}
+    create(req,res){
+        return res.render ('instructors/create')
+    },
 
-//post
-exports.post = function (req, res) {
-    const keys = Object.keys(req.body)
-    for (key of keys) {
-        if (req.body[key] == "") {
-            return res.send('Por favor, preencha todos os campos!')
+    post(req,res){
+        const keys = Object.keys(req.body)
+        for (key of keys) {
+            if (req.body[key] == "") {
+                return res.send('Por favor, preencha todos os campos!')
+            }
         }
-    }
+        let { avatar_url, birth, name, services, gender } = req.body
+        return
+    },
 
-    let { avatar_url, birth, name, services, gender } = req.body
+    show(req,res){
+        return
+    },
 
+    edit(req,res){
+        return
+    },
 
-    birth = Date.parse(birth)
-    const created_at = Date.now()
-    const id = Number(data.instructors.length + 1)
-
-
-    data.instructors.push({
-        id,
-        name,
-        avatar_url,
-        birth,
-        created_at,
-        gender,
-        services
-    })
-
-    fs.writeFile("data.json", JSON.stringify(data, null, 2), function (err) {
-        if (err) return res.send("Erro na gravação!")
-
-        return res.redirect("/instructors")
-    })
-
-    // return res.send(req.body)
-}
-
-
-//show
-exports.show = function (req, res) {
-    // req.params
-    const { id } = req.params
-
-    const foundInstructor = data.instructors.find(function (instructor) {
-        return instructor.id == id
-    })
-
-    if (!foundInstructor) return res.send("Instrutor não encontrado!")
-
-    const instructor = {
-        ...foundInstructor,
-        age: age(foundInstructor.birth),
-        // o split serve para pegar uma string e transformar em um array
-        services: foundInstructor.services.split(","),
-        // formatacao da data - tive que instalar o intl no npm e importar ele no arquivo para resolver o bug
-        created_at: new Intl.DateTimeFormat("pt-BR").format(foundInstructor.created_at),
-    }
-
-    return res.render("instructors/show", { instructor })
-}
-
-//edit (pagina para editar) - Mostrando os dados no front-end para a edição
-exports.edit = function(req, res) {
-    const { id } = req.params
-
-    const foundInstructor = data.instructors.find(function (instructor) {
-        return instructor.id == id
-    })
-
-    if (!foundInstructor) return res.send("Instrutor não encontrado!")
-
-    const instructor = {
-        ...foundInstructor,
-        birth: date(foundInstructor.birth).iso
-    }
-
-    return res.render('instructors/edit', {instructor})
-}
-
-//put (salvar a alteracao no back-end)
-exports.put = function (req, res) {
-    const { id } = req.body
-    let index = 0
-
-    const foundInstructor = data.instructors.find(function (instructor, foundIndex) {
-        if (id == instructor.id) {
-            index = foundIndex
-            return true
+    put(req,res){
+        const keys = Object.keys(req.body)
+        for (key of keys) {
+            if (req.body[key] == "") {
+                return res.send('Por favor, preencha todos os campos!')
+            }
         }
-    })
 
-    if (!foundInstructor) return res.send("Instrutor não encontrado!")
+        return
+    },
 
-    const instructor = {
-        ...foundInstructor,
-        ...req.body,
-        birth: Date.parse(req.body.birth),
-        id: Number(req.body.id)
+    delete(req,res){
+        return
     }
-
-    data.instructors[index] = instructor
-    
-    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
-        if(err) return res.send("Escreva o erro!")
-
-        return res.redirect(`/instructors/${ id }`)
-    })
-}
-
-//delete
-exports.delete = function (req, res) {
-    const { id } = req.body
-
-    const filteredInstructors = data.instructors.filter(function(instructor){
-        return instructor.id != id
-    })
-
-    data.instructors = filteredInstructors
-
-    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
-        if (err) return res.send("Escreva o erro!")
-
-        return res.redirect("/instructors")
-    })
 }
--- scr/lib/utils.js
+++ scr/lib/utils.js
@@ -1,5 +1,5 @@
 module.exports = {
-  age: function (timestamp) {
+  age (timestamp) {
     const today = new Date();
     const birthDate = new Date(timestamp);
 
@@ -13,15 +13,8 @@ module.exports = {
     return age;
   },
 
-  // convertendo o formato de milisegundo por yyyy-mm-dd   
-  date: function(timestamp) {
-    //date(instructor.birth)
-    const date = new Date (timestamp)  
-    //instructor.birth = 09329832837287483748739483
-    //date(instructor.bith)
-    //return yyyy-mm-dd
-
-    //o UTC deixa a data universal 
+  date(timestamp) {
+    const date = new Date (timestamp) 
     const year = date.getUTCFullYear()
     const month = `0${date.getUTCMonth() + 1}`.slice(-2)
     const day = `0${date.getUTCDate ()}`.slice(-2)
