c9adbbd77ba8e09d71cabe1dc2e2bda8ecfb96a4
Priscila Ribeiro <priscilt.ribeiro@hotmail.com>
2020-12-01 17:47:08 -0300

    configurando dados no banco

---
+++ scr/config/db.js
@@ -0,0 +1,13 @@
+// conexão com o banco de dados
+
+// o pool permite que acesse os dados sem precisar colocar o login toda hora, é tipo uma configuração que guarda a informacao que pode deixar o usuario acessar
+const { Pool } = require("pg")
+
+// credenciais do banco de dado
+module.exports = new Pool({
+    user:"priscilaribeiro",
+    password:"",
+    host: "localhost",
+    port:5432,
+    database:"gymmanger"
+})
