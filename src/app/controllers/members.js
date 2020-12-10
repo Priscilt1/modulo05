5482dbf9407656b3e2e4862d953ed29be56edf07
Priscila Ribeiro <priscilt.ribeiro@hotmail.com>
2020-12-10 09:18:25 -0300

    selecionando instrutor na pagina de edição

--- src/app/controllers/members.js
+++ src/app/controllers/members.js
@@ -44,7 +44,10 @@ module.exports = {
 
             member.birth = date(member.birth).iso
 
-            return res.render("members/edit", {member})
+            Member.instructorsSelectOptions(function(options){
+                return res.render ('members/edit', { member, instructorOptions: options })
+            })
+
         })
     },
 
